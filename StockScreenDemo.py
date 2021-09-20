611e96b3f6070073eb13c702be2e07f22bd3ae21
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-09-20 19:19:05 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -6,36 +6,3 @@ import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-option = st.sidebasr.selectbox("Dashboard", ("Cartera","Stocktwists","Twitter", "Seguimiento"))
-
-st.header(option)
-
-if option == "Cartera":
-	st.subheader("Cartera")
-	
-if option == "Seguimiento":
-	st.subheader("Seguimiento")
-
-	st.write("""
-		# Stock Price
-		""")
-
-	
-
-	tickerData = yf.Ticker(tickerSymbol) 
-	# get the historical prices for the ticker
-	tickerDf = tickerData.history(period='1d', start='2015-5-31', end='2021-07-21')
-
-	st.line_chart(tickerDf.Close)
-	st.line_chart(tickerDf.Volume)
-
-
-
-if option == "Stocktwists":
-	st.subheader("Stocktwists")
-
-if option == "Twitter":
-	st.subheader("Twitter")
-
-    
- 
