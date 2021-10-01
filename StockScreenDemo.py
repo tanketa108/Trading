6587b7de517652c4803518d8eca645ec2478eeb5
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-01 19:56:46 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -12,6 +12,8 @@ import altair as alt
 # Welcome to Streamlit!
 
 """
+data = []
+
 st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
