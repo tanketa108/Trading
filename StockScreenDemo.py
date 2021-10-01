97e5cd190dae5bab1853346be4fc3d066b64eddc
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-01 20:06:50 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -16,8 +16,24 @@ import altair as alt
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Portfolio", "Information", "News")
-    if st.sidebar.selectbox = Portfolio
-    (
+    
+    st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+    
         data = []
 
 st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
@@ -25,7 +41,7 @@ st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .encode(x='x:Q', y='y:Q'))
     )
     
-)
+
 
 
 
