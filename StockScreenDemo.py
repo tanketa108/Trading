da6cd200419eb95755118c7ffaed0406d4496bd5
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-01 19:52:55 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -10,4 +10,9 @@ from plotly.subplots import make_subplots
 # Welcome to Streamlit!
 
 """
->>> st.markdown('Streamlit is **_really_ cool**.')
+streamlit.area_chart(data=None, width=0, height=0, use_container_width=True)
+>>> chart_data = pd.DataFrame(
+...     np.random.randn(20, 3),
+...     columns=['a', 'b', 'c'])
+...
+>>> st.area_chart(chart_data)
