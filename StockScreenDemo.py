e3ef6e6283a0bb33d7d87735230c874350e51172
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-04 14:49:20 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -22,6 +22,11 @@ if add_selectbox == "Portfolio":
 
 else
 
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
+
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'GOOGL'
@@ -31,9 +36,14 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
-    
         data = []
 
 st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
