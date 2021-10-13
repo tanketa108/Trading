e7d86f649e4263a35ddac95b348e8e48036efadd
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-13 15:05:51 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -7,6 +7,12 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import altair as alt
 
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
 
 """
 # Welcome to Streamlit!
@@ -21,39 +27,6 @@ if add_selectbox == "Portfolio":
 	st.subheader("Portfolio")
 
 else
-"""
-
-st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Google!
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
-"""
-
-#	data = []
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-    )
-    
 """
 
 
