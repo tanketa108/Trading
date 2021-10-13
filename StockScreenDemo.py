fcef6e39ef8e47d1b956df6604fecf02125dac1a
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-13 15:10:47 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -14,13 +14,8 @@ df = pd.DataFrame({
 
 df
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+
+chart_data = pd.DataFrame()
 
 st.line_chart(chart_data)
 
