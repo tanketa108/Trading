4979f80ad2b35a134f9c876f363cecba2c8155e0
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-13 15:26:59 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -6,6 +6,8 @@ import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import altair as alt
+import numpy as np
+import time
 
 df = pd.DataFrame({
      'first column': [1, 2, 3, 4],
@@ -15,14 +17,23 @@ df
 chart_data = pd.DataFrame()
 st.line_chart(chart_data)
      
-     
+
+# Get some data.
+data = np.random.randn(10, 2)
+
+# Show the data as a chart.
+chart = st.line_chart(data)
+
+# Wait 1 second, so the change is clearer.
+time.sleep(1)
+
+# Grab some more data.
+data2 = np.random.randn(10, 2)
+
+# Append the new data to the existing chart.
+chart.add_rows(data2)
 option = st.selectbox(
      '',
      ('Portfolio', 'Market information', 'Portfolio creator'))
 st.write('You selected:', option)
 
-if st.selectbox('Portfolio'):
-     ## Portfolio
-     
-     ##
- else
