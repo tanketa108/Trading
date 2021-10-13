29d3946212bb9050d0b731bcaceaf32440f94e00
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-13 15:09:27 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -14,6 +14,10 @@ df = pd.DataFrame({
 
 df
 
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
