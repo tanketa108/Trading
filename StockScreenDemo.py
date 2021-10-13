c7aa303b14247aaa31c96fb220e9c4171758f8bb
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-13 15:21:22 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -7,17 +7,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import altair as alt
 
-option = st.selectbox(
-     '',
-     ('Portfolio', 'Market information', 'Portfolio creator'))
-st.write('You selected:', option)
-
-if st.selectbox('Portfolio'):
-     ## Portfolio
-     
-     ##
-if st.selectbox('Market information'):
-     df = pd.DataFrame({
+df = pd.DataFrame({
      'first column': [1, 2, 3, 4],
      'second column': [10, 20, 30, 40]
      })
@@ -28,6 +18,19 @@ if st.selectbox('Market information'):
      chart_data = pd.DataFrame()
 
      st.line_chart(chart_data)
+     
+     
+option = st.selectbox(
+     '',
+     ('Portfolio', 'Market information', 'Portfolio creator'))
+st.write('You selected:', option)
+
+if st.selectbox('Portfolio'):
+     ## Portfolio
+     
+     ##
+if st.selectbox('Market information'):
+     
 
 
 
