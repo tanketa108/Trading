9b7ceca0332f621d7a98099f03062d09f5372fcb
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-18 18:28:43 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -23,8 +23,8 @@ with header:
 
 with Portfolio:
      st.header('Patrimonio')
-     #patrimonio = pd.read_csv('.C:/Python/Trading/data/Patrimonio.csv')
-     #st.write('.C:/Python/Trading/data/Patrimonio.csv')
+     patrimonio = pd.read_csv('https://github.com/tanketa108/Trading/blob/main/data/Patrimonio.csv')
+     st.write('.C:/Python/Trading/data/Patrimonio.csv')
      
      df = pd.DataFrame({
         
