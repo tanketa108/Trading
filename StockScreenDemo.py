e578dd7d8c6d0e68d01f5bc9f73077a66e8d8fb5
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-18 15:36:07 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -25,12 +25,11 @@ with Portfolio:
      #st.write('patrimonio.csv')
      
      df = pd.DataFrame({
-          
-     'Fecha': [04.08.2020,28.08.2020,12.09.2020,04.11.2020],
-     'Saldo broker': [10,20,30,40]
-     'Criptomonedas': [0,0,131.06,122.23]
-     'Saldo cuenta': [218,244.52,327.52,270.01]
-     })
+          'Fecha': [04.08.2020, 28.08.2020, 12.09.2020, 04.11.2020],
+          'Saldo broker': [10, 20, 30, 40]
+          'Criptomonedas': [0, 0, 131.06, 122.23]
+          'Saldo cuenta': [218 ,244.52, 327.52, 270.01]
+          })
      df
      chart_data = pd.DataFrame()
      st.line_chart(chart_data)
