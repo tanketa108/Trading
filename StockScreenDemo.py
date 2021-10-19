af16a5314957a88cb7965398b71bb377592bcdd6
tanketa108 <86806331+tanketa108@users.noreply.github.com>
2021-10-19 19:08:17 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -26,7 +26,12 @@ if option == "Portfolio":
 
 if option== "Market Information":
      st.subheader(option)
-     title = st.text_input('Movie title', 'Life of Brian')
+     title = st.text_input('APP', 'BAC')
+     if title == "APP":
+          st.subheader(Hi APP)
+     if title == "BAC":
+          st.subheader(Hi BAC)
+          
      simbol = "APP"
      st.image(f"http://finviz.com/chart.ashx?t={simbol}")
 
